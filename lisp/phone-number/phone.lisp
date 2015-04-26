e2ffeee5bb6a51d72e14318fccefcc42e1186beb
Josh Carton <josh.carton@gmail.com>
2015-04-26 22:20:08 +0000

    updated Phone-Number for less-cryptic naming

--- lisp/phone-number/phone.lisp
+++ lisp/phone-number/phone.lisp
@@ -5,27 +5,26 @@
 
 (in-package #:phone)
 
-(defun trim-number (num-str)
-  (if (= (length num-str) 10)
-      num-str
-      (subseq num-str 1)))
+(defun remove-nans (str)
+  (remove-if-not #'digit-char-p str))
 
 (defun ensure-phone-number (str)
-  (let ((num-str (remove-if-not #'digit-char-p str)))
-    (if (or (= (length num-str) 10)
-                  (and (= (length num-str) 11)
-                       (char= #\1 (char num-str 0))))
-        (trim-number num-str)
-        "0000000000")))
-        
+  (let* ((digits (remove-nans str))
+         (len (length digits)))
+    (cond ((= 10 len) digits)
+          ((and (= 11 len) 
+                (char= #\1 (char digits 0)))
+             (subseq digits 1))
+          (t "0000000000"))))
+
 (defun numbers (phone-number)
   (ensure-phone-number phone-number))
 
 (defun pretty-print (phone-number)
-  (let ((number (ensure-phone-number phone-number)))
-    (format nil "(~a) ~a-~a" (subseq number 0 3) 
-                           (subseq number 3 6)
-                           (subseq number 6))))
+  (let ((digits (ensure-phone-number phone-number)))
+    (format nil "(~a) ~a-~a" (subseq digits 0 3) 
+                             (subseq digits 3 6)
+                             (subseq digits 6))))
 
 (defun area-code (phone-number)
   (subseq (ensure-phone-number phone-number) 0 3))
