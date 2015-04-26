542fd29312b86890111c4db087722975957d5f0b
DruidGreeneyes <josh.carton@gmail.com>
2015-04-26 13:03:28 -0500

    Triangle: first attempt

--- lisp/triangle/triangle.lisp
+++ lisp/triangle/triangle.lisp
@@ -5,7 +5,15 @@
 
 (in-package #:triangle)
 
-(defun valid-triangle-p (triangle)
-  (let ((max-side (/ (apply #'sum triangle) 2)))
+(defun invalid-triangle-p (triangle)
+  (let ((max-side (/ (apply #'+ triangle) 2)))
     (find-if #'(lambda (n) (>= n max-side)) triangle)))
+
+(defun triangle (side-1 side-2 side-3)
+  (cond ((= side-1 side-2 side-3) :equilateral)
+	((invalid-triangle-p (list side-1 side-2 side-3)) :illogical)
+	((or (= side-1 side-2)
+	     (= side-2 side-3)
+	     (= side-3 side-1)) :isosceles)
+	(t :scalene)))
 
