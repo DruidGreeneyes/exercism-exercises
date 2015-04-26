1ff7a4ea50264e90e8755182f8ea89181fdedffc
Josh Carton <josh.carton@gmail.com>
2015-04-26 22:28:39 +0000

    updated Phone-Number for functional style (I think?)

--- lisp/phone-number/phone.lisp
+++ lisp/phone-number/phone.lisp
@@ -8,23 +8,24 @@
 (defun remove-nans (str)
   (remove-if-not #'digit-char-p str))
 
-(defun ensure-phone-number (str)
-  (let* ((digits (remove-nans str))
-         (len (length digits)))
+(defun ensure-phone-number (digits)
+  (let ((len (length digits)))
     (cond ((= 10 len) digits)
           ((and (= 11 len) 
                 (char= #\1 (char digits 0)))
              (subseq digits 1))
           (t "0000000000"))))
+          
+(defun prettify-digits (digits)
+  (format nil "(~a) ~a-~a" (subseq digits 0 3) 
+                           (subseq digits 3 6)
+                           (subseq digits 6)))
 
 (defun numbers (phone-number)
-  (ensure-phone-number phone-number))
+  (ensure-phone-number (remove-nans phone-number)))
 
 (defun pretty-print (phone-number)
-  (let ((digits (ensure-phone-number phone-number)))
-    (format nil "(~a) ~a-~a" (subseq digits 0 3) 
-                             (subseq digits 3 6)
-                             (subseq digits 6))))
+  (prettify-digits (numbers phone-number)))
 
 (defun area-code (phone-number)
-  (subseq (ensure-phone-number phone-number) 0 3))
+  (subseq (numbers phone-number) 0 3))
