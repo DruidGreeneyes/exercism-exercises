9bd7f545d0373fc027596c39a483919b9671897d
Josh Carton <josh.carton@gmail.com>
2015-04-29 20:30:17 +0000

    Updated meetup to catch leap years

--- lisp/meetup/meetup.lisp
+++ lisp/meetup/meetup.lisp
@@ -9,19 +9,14 @@
   (nth-value 6 (decode-universal-time
                  (encode-universal-time 0 0 0 date month year))))
 
-(defun find-month-end (month)
+(defun find-month-end (month year)
   (let ((mod-month (1+ (mod month 12))))
     (nth-value 3 (decode-universal-time
-              (- (encode-universal-time 0 0 0 1 mod-month 1901) 1)))))
+              (- (encode-universal-time 0 0 0 1 mod-month year) 1)))))
 
-(defun get-month-list (month)
-  (loop for n from 1 to (find-month-end month)
+(defun get-month-list (month year)
+  (loop for n from 1 to (find-month-end month year)
         collect n))
-
-(defun filter-month (month year day-key)
-  (let ((month-list (get-month-list month))
-        (dow (translate-day day-key)))
-    (remove-if-not #'(lambda (x) (eql (get-day-of-week x month year) dow)) month-list)))
 
 (defun translate-day (day)
   (ccase day (:monday 0)
@@ -32,6 +27,11 @@
              (:saturday 5)
              (:sunday 6)))
 
+(defun filter-month (month year day-key)
+  (let ((month-list (get-month-list month year))
+        (dow (translate-day day-key)))
+    (remove-if-not #'(lambda (x) (eql (get-day-of-week x month year) dow)) month-list)))
+    
 (defun find-nth-day (n-symbol days)
   (if (eql n-symbol :teenth)
       (find-if #'(lambda (n) (< 12 n 20)) days)
@@ -41,10 +41,10 @@
                       (:fourth (fourth days))
                       (:last (car (last days))))))
 
-(defun find-nth-day-new (n-symbol days)
-  (if (eql n-symbol :teenth)
-      (find-if #'teen-p days)
-      (apply (intern (string n-symbol)) days)))
+;;(defun find-nth-day-new (n-symbol days)
+;;  (if (eql n-symbol :teenth)
+;;      (find-if #'teen-p days)
+;;      (apply (intern (string n-symbol)) days)))
   
 (defun meetup (month year &optional (day-key :monday) (nth-key :first))
   (let ((possible-days (filter-month month year day-key)))
