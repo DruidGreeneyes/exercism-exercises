fbe13438d6ef8c0b4cea5d1afddfe20bc6c04ffc
Josh Carton <josh.carton@gmail.com>
2015-04-30 22:06:39 +0000

    trying to get space-age to work using (fdefinition)

--- lisp/space-age/space-age.lisp
+++ lisp/space-age/space-age.lisp
@@ -6,20 +6,25 @@
 (in-package #:space-age)
 
 (defconstant s (float 31557600))
+(defconstant +planets+ (list ("earth" . 1)
+                             ("mercury" . 0.2408467)
+                             ("venus" . 0.61519726)
+                             ("mars" . 1.8808158)
+                             ("jupiter" . 11.862615)
+                             ("saturn" . 29.447498)
+                             ("uranus" . 84.016846)
+                             ("neptune" . 164.79132)))
 
 (defun round-to-two (n)
   (and (floatp n)
        (/ (round (* n 100)) 100)))
+       
+(defun def-on-planet (planet multiplier)
+  (let ((name (concatenate 'string "on-" planet))
+        (secs-per-year (* s multiplier)))
+    (setf (fdefinition planet)
+          (lambda (secs) 
+            (round-to-two (/ secs secs-per-year))))))
 
-(defmacro def-on-thing (name secs-per-year)
-  `(defun ,name (secs)
-     (round-to-two (/ secs ,secs-per-year))))
-
-(def-on-thing on-earth s)
-(def-on-thing on-mercury (* s 0.2408467))
-(def-on-thing on-venus (* s 0.61519726))
-(def-on-thing on-mars (* s 1.8808158))
-(def-on-thing on-jupiter (* s 11.862615))
-(def-on-thing on-saturn (* s 29.447498))
-(def-on-thing on-uranus (* s 84.016846))
-(def-on-thing on-neptune (* s 164.79132))
+(loop for p in +planets+ do
+  (def-on-planet (car p) (cdr p)))
