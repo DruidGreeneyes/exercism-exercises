ee2672de853a26c9de53b11e66e8b01d0d3c44e3
Josh Carton <josh.carton@gmail.com>
2015-05-01 20:08:46 +0000

    Further refinements to space-age

--- lisp/space-age/space-age.lisp
+++ lisp/space-age/space-age.lisp
@@ -6,22 +6,14 @@
 (in-package #:space-age)
 
 (defconstant s (float 31557600))
-(defconstant +planets+ (list "earth" 
-                             "mercury"
-                             "venus"
-                             "mars"
-                             "jupiter"
-                             "saturn" 
-                             "uranus"
-                             "neptune"))
-(defconstant +multipliers+ (list 1 
-                                 0.2408467
-                                 0.61519726
-                                 1.8808158
-                                 11.862615
-                                 29.447498
-                                 84.016846
-                                 164.79132))
+(defconstant +planets+ (list "EARTH" 1
+                             "MERCURY" 0.2408467
+                             "VENUS" 0.61519726
+                             "MARS" 1.8808158
+                             "JUPITER" 11.862615
+                             "SATURN" 29.447498
+                             "URANUS" 84.016846
+                             "NEPTUNE" 164.79132))
 
 (defun round-to-two (n)
   (and (floatp n)
@@ -32,6 +24,6 @@
     (lambda (secs)
       (round-to-two (/ secs secs-per-year)))))
 
-(loop for p in (pairlis +planets+ +multipliers+) do
-      (let ((name (intern (concatenate 'string "ON-" (string-upcase (car p))))))
-        (setf (fdefinition name) (def-on-planet (cdr p)))))
+(loop for p on +planets+ by #'cddr
+      do (let ((name (intern (concatenate 'string "ON-" (car p)))))
+           (setf (fdefinition name) (def-on-planet (cadr p)))))
