ff1bad6e5775430a3b7bdcbcf6e7764961354003
DruidGreeneyes <josh.carton@gmail.com>
2015-05-06 10:02:52 -0500

    Updated bob for functionalism.

--- lisp/bob/bob.lisp
+++ lisp/bob/bob.lisp
@@ -11,25 +11,14 @@
   (find-if #'alpha-char-p str))
 
 (defun yelling-p (str)
-  (string= str (string-upcase str)))
+  (and (has-alpha-chars-p input)
+       (string= str (string-upcase str))))
 
 (defun question-p (str)
   (char= #\? (char str (- (length str) 1))))
 
 (defun response-for (input)
   (cond ((silence-p input) "Fine. Be that way!")
-        ((and (has-alpha-chars-p input) 
-              (yelling-p input)) "Whoa, chill out!")
+        ((yelling-p input) "Whoa, chill out!")
         ((question-p input) "Sure.")
         (t "Whatever.")))
-
-;(defun response-for (input) 
-;  (if (equal "" (string-trim " " input))
-;      "Fine. Be that way!"
-;      (if (and (find-if #'alpha-char-p input)
-;               (equal input (string-upcase input)))
-;          "Whoa, chill out!"
-;          (if (equal #\? (char input (- (length input) 1)))
-;              "Sure."
-;              "Whatever."))))
-;      
