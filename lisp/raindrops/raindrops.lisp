8d8ae2d3863ed080201af8f2ff278542eff6eecc
DruidGreeneyes <josh.carton@gmail.com>
2015-10-25 10:22:47 +0000

    used destructuring-bind for better alist comprehension

--- lisp/raindrops/raindrops.lisp
+++ lisp/raindrops/raindrops.lisp
@@ -13,13 +13,23 @@
 (defun has-factor? (n i)
   (= 0 (mod n i)))
 
-(defun generate-sounds (n alst)
+(defun add-sound (str snd)
+  (concatenate 'string str snd))
+
+(defun generate-sounds (n alst &optional (str nil))
+  (if (atom alst) str
+      (destructuring-bind ((i . snd) . rest) alst
+        (generate-sounds n rest (if (has-factor? n i)
+                                    (add-sound str snd)
+                                    str)))))
+
+(defun generate-sounds-old (n alst)
   (and (consp alst)
        (if (has-factor? n (caar alst))
-		   (concatenate 'string
+           (concatenate 'string
                         (cdar alst)
                         (generate-sounds n (cdr alst)))
-		   (generate-sounds n (cdr alst)))))
+           (generate-sounds n (cdr alst)))))
 
 (defun convert (n)
   (let ((sounds (generate-sounds n *sounds*)))
